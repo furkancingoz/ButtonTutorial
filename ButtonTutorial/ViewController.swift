29750f17b8e2e4526334d8cdca956b55c589951a
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-01-15 21:42:37 +0300

    start

--- ButtonTutorial/ViewController.swift
+++ ButtonTutorial/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+//  private let button: UIButton = {
+//    let button = UIButton()
+//
+//    return button
+//  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
